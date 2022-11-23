0c919d193fce7d18af4e0851b0b35a40ebd4b319
Matheus Paul <matheuspaulcoelho@hotmail.com>
2022-11-23 16:30:39 -0300

    exercicio3

---
+++ fundamentos/secao-4-introducao-a-javascript/dia-1-primeiros-passos/exercicio3/script.js
@@ -0,0 +1,8 @@
+const nota = 50
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)")
+} else if (nota >= 60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovada(a)")
+}

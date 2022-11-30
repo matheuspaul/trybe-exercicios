62b5ef1cbb088f0ed8ebaa2fbc3e69c0d07e94f4
Matheus Paul <matheuspaulcoelho@hotmail.com>
2022-11-30 15:27:44 -0300

    mensagem3 dada

--- fundamentos/secao-4-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios1/scripts.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios1/scripts.js
@@ -12,3 +12,5 @@ let player ={
 console.log('A jogadora ' + [player.name] + ' ' + [player.lastName] + ' tem ' + [player.age] + ' anos de idade.');
 
 console.log('A jogadora ' + [player.name] + ' ' + [player.lastName] + ' foi eleita a melhor do mundo por ' + [player.bestInTheWorld.length] + ' vezes.');
+
+console.log('A jogadora possui ' + [player.medals['golden']] + ' medalhas de ouro e ' + [player.medals['silver']] + ' medalhas de prata.')

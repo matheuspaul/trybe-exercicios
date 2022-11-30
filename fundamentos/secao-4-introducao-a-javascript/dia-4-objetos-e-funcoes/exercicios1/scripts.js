e1a2bb8cace50a5e2c1ffc8c16de0a6dc26810f0
Matheus Paul <matheuspaulcoelho@hotmail.com>
2022-11-30 15:24:27 -0300

    mensagem dada 2

--- fundamentos/secao-4-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios1/scripts.js
+++ fundamentos/secao-4-introducao-a-javascript/dia-4-objetos-e-funcoes/exercicios1/scripts.js
@@ -2,6 +2,7 @@ let player ={
     name: 'Marta',
     lastName: 'Silva',
     age: 34,
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
     medals: {
         'golden': 2,
         'silver': 3
@@ -9,3 +10,5 @@ let player ={
 };
 
 console.log('A jogadora ' + [player.name] + ' ' + [player.lastName] + ' tem ' + [player.age] + ' anos de idade.');
+
+console.log('A jogadora ' + [player.name] + ' ' + [player.lastName] + ' foi eleita a melhor do mundo por ' + [player.bestInTheWorld.length] + ' vezes.');

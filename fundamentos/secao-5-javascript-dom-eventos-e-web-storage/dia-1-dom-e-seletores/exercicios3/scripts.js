51e0b986d16547506030a9a498e9b7e9721c6695
Matheus Paul <matheuspaulcoelho@hotmail.com>
2022-12-08 00:05:13 -0300

    primeira função ok

---
+++ fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-dom-e-seletores/exercicios3/scripts.js
@@ -0,0 +1,6 @@
+const changeText = () => {
+    let paragraph = document.getElementsByTagName('p')[1];
+    paragraph.innerHTML = 'Quero ter minha própria residência e trabalhar numa empresa que me pague bem!'
+}
+changeText()
+

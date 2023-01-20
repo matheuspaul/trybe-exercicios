cacb081a2320cdaed7972f00f007df9af0b5635e
Matheus Paul <matheuspaulcoelho@hotmail.com>
2023-01-20 17:54:51 -0300

    adicionado describe e it

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/exercicios1/sum.test.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/exercicios1/sum.test.js
@@ -1,5 +1,7 @@
 // sum.test.js
 const sum = require('./sum');
-test('sums two values', () => {
-  expect(sum(4, 5)).toBe(9);
-});
+describe('Function sum ok', () => {
+    it('sums two values', () => {
+        expect(sum(4, 5)).toBe(9);
+    });
+})

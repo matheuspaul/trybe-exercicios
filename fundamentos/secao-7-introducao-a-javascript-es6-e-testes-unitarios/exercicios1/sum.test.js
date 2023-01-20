e213c8305c00cbc6a46012b6a7677e2187ebf462
Matheus Paul <matheuspaulcoelho@hotmail.com>
2023-01-20 18:05:05 -0300

    exercicio 1 ok

--- fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/exercicios1/sum.test.js
+++ fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/exercicios1/sum.test.js
@@ -1,7 +1,12 @@
 // sum.test.js
 const sum = require('./sum');
 describe('Function sum ok', () => {
-    it('sums two values', () => {
+    it('deve retornar o resultado da soma', () => {
         expect(sum(4, 5)).toBe(9);
+        expect(sum(0, 0)).toBe(0);
     });
-})
+    it('deve dispara um erro caso receba como parametro uma string', () => {
+        expect(() => sum(4, '5')).toThrowError();
+        expect(() => sum(4, '5')).toThrowError('Parameters must be numbers');
+    });
+});
